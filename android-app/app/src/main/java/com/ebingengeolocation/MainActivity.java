7b8dc53e9b5bf8e1c359d55797ea3f27cd578a31
Marcel Freiberg <marcel.freiberg@outlook.com>
2019-11-28 20:30:12 +0100

    Retrieve gps data and display it on display

--- android-app/app/src/main/java/com/ebingengeolocation/MainActivity.java
+++ android-app/app/src/main/java/com/ebingengeolocation/MainActivity.java
@@ -1,14 +1,74 @@
 package com.ebingengeolocation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int PERM_REQUEST_CODE = 69;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView gpsCoordinates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        gpsCoordinates = findViewById(R.id.gps_coordinates);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERM_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                retrieveGpsData();
+            }
+        }
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        PERM_REQUEST_CODE);
+            }
+        }
+    }
+
+    private void retrieveGpsData() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            String latitudeStr = Double.toString(location.getLatitude());
+                            String longitudeStr = Double.toString(location.getLongitude());
+
+                            gpsCoordinates.setText(latitudeStr + ", " + longitudeStr);
+                        }
+                    }
+                });
     }
 }
